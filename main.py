a45073c8b4f815753c2f01b736c7a61e41691579
rogcg <Roger@Roger.local>
2016-08-31 09:40:46 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#/usr/bin/env/python
+
+from decimal import *
+
+print "==== ENVIRONMENT ===="
+
+world_name = raw_input('Enter world name: ')
+world_size = raw_input('Enter world size in km: ')
+
+print("%s - %s" % (world_name, world_size))
+
